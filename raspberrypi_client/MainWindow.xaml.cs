c6ec041cb0410cf55850d1364fb7d38b936e8276
MxinShangshang <jiangmingxin825@live.cn>
2015-08-30 16:03:33 +0800

    before change
    20150830

--- raspberrypi_client/MainWindow.xaml.cs
+++ raspberrypi_client/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace raspberrypi_client
     {
         Socket c;
         private Thread Listen;
+        private Thread Self_send;
         ObservableDataSource<Point> source1 = null;
         ObservableDataSource<Point> source2 = null;
         ObservableDataSource<Point> source3 = null;
@@ -66,6 +67,7 @@ namespace raspberrypi_client
             //}
             ipe = new IPEndPoint(ip, port);//把ip和端口转化为IPEndPoint实例
             c = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//创建一个Socket
+            c.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
             this.richTextBox.Dispatcher.Invoke(new WriteDelegate(ShowText), "连接到服务端...");
             c.Connect(ipe);//连接到服务器
             Listen = new Thread(new ThreadStart(this.StartListen));
@@ -77,47 +79,46 @@ namespace raspberrypi_client
         {
             CultureInfo culture = CultureInfo.InvariantCulture;
             Assembly executingAssembly = Assembly.GetExecutingAssembly();
-            try
-            {
+            //try
+            //{
                 string recvStr = "";
-                byte[] recvBytes = new byte[64];
+                byte[] recvBytes = new byte[1024];
                 int bytes = 0;
                 int num = 0;
-                while (true)
+            while (true)
+            {
+                bytes = c.Receive(recvBytes, recvBytes.Length, 0);//从服务器端接受返回信息
+                if (bytes > 0)
                 {
-                    bytes = c.Receive(recvBytes, recvBytes.Length, 0);//从服务器端接受返回信息
-                    if (bytes >= 0)
-                    {
-                        recvStr += Encoding.Default.GetString(recvBytes, 0, bytes).Replace("\0", "");
-                        this.richTextBox.Dispatcher.Invoke(new WriteDelegate(ShowText), "服务器返回信息：" + recvStr);
-                        string line = recvStr.Replace("  "," ");
-                        string[] values = recvStr.Split(' ');//new string[4];//line.Split(',');
-                        values[0] = (num++).ToString();
+                    recvStr += Encoding.ASCII.GetString(recvBytes, 0, bytes);
+                    this.richTextBox.Dispatcher.Invoke(new WriteDelegate(ShowText), "服务器返回信息：" + recvStr);
+                    string[] values = recvStr.Split(' ');//new string[4];//line.Split(',');
+                    values[0] = (num++).ToString();
 
-                        double x = Double.Parse(values[0], culture);
-                        double y1 = Double.Parse(values[2], culture);
-                        double y2 = Double.Parse(values[4], culture);
-                        double y3 = Double.Parse(values[6], culture);
-                        double y4 = Double.Parse(values[8], culture);
+                    //double x = Double.Parse(values[0], culture);
+                    //double y1 = Double.Parse(values[1], culture);
+                    //double y2 = Double.Parse(values[2], culture);
+                    //double y3 = Double.Parse(values[3], culture);
+                    //double y4 = Double.Parse(values[4], culture);
 
-                        Point p1 = new Point(x, y1);
-                        Point p2 = new Point(x, y2);
-                        Point p3 = new Point(x, y3);
-                        Point p4 = new Point(x, y4);
+                    //Point p1 = new Point(x, y1);
+                    //Point p2 = new Point(x, y2);
+                    //Point p3 = new Point(x, y3);
+                    //Point p4 = new Point(x, y4);
 
-                        source1.AppendAsync(Dispatcher, p1);
-                        source2.AppendAsync(Dispatcher, p2);
-                        source3.AppendAsync(Dispatcher, p3);
-                        source4.AppendAsync(Dispatcher, p4);
+                    //source1.AppendAsync(Dispatcher, p1);
+                    //source2.AppendAsync(Dispatcher, p2);
+                    //source3.AppendAsync(Dispatcher, p3);
+                    //source4.AppendAsync(Dispatcher, p4);
 
-                        recvStr = null;
-                    }
+                    recvStr = "";
                 }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
         }
 
         private void connect_Click(object sender, RoutedEventArgs e)
@@ -125,6 +126,9 @@ namespace raspberrypi_client
             try
             {
                 InitClient();
+                //Self_send = new Thread(self_send);
+                //Self_send.IsBackground = true;
+                //Self_send.Start();
             }
             catch (Exception ex)
             {
@@ -132,11 +136,22 @@ namespace raspberrypi_client
             }
         }
 
+        private void self_send()
+        {
+            while (true)
+            {
+                string sendStr = "1";
+                byte[] bs = Encoding.ASCII.GetBytes(sendStr);
+                c.Send(bs, bs.Length, 0);
+                Thread.Sleep(1000);
+            }
+        }
+
         private void send_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                this.richTextBox.Dispatcher.Invoke(new WriteDelegate(ShowText), "发送消息到服务端 "+ sendMessage.Text);
+                this.richTextBox.Dispatcher.Invoke(new WriteDelegate(ShowText), "发送消息到服务端 " + sendMessage.Text);
                 string sendStr = sendMessage.Text;
                 byte[] bs = Encoding.ASCII.GetBytes(sendStr);
                 c.Send(bs, bs.Length, 0);
@@ -164,18 +179,20 @@ namespace raspberrypi_client
         private void ShowText(string text)
         {
             this.richTextBox.AppendText(text + "\n");
+            this.richTextBox.ScrollToEnd();
 
         }
 
 
         private void close_Click(object sender, RoutedEventArgs e)
         {
-            c.Close();
             if (Listen != null)
             {
+                c.Close();
                 if (Listen.ThreadState == ThreadState.Running)
                 {
                     Listen.Abort();
+                    //Self_send.Abort();
                 }
             }
             System.Diagnostics.Process.GetCurrentProcess().Kill();
