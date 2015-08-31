7087b65f977adc1a191b04dadac533c89b2a58f0
MxinShangshang <jiangmingxin825@live.cn>
2015-08-31 16:03:30 +0800

    add ftp transmission
    20150831

--- raspberrypi_client/MainWindow.xaml.cs
+++ raspberrypi_client/MainWindow.xaml.cs
@@ -6,9 +6,9 @@ using System.Globalization;
 using System.Threading;
 using System.Net;
 using System.Net.Sockets;
-using System.IO;
 using Microsoft.Research.DynamicDataDisplay;
 using Microsoft.Research.DynamicDataDisplay.DataSources;
+using System.IO;
 
 namespace raspberrypi_client
 {
@@ -19,7 +19,6 @@ namespace raspberrypi_client
     {
         Socket c;
         private Thread Listen;
-        private Thread Self_send;
         ObservableDataSource<Point> source1 = null;
         ObservableDataSource<Point> source2 = null;
         ObservableDataSource<Point> source3 = null;
@@ -57,43 +56,61 @@ namespace raspberrypi_client
             string host = xip.Text;
             IPAddress ip = IPAddress.Parse(host);
             IPEndPoint ipe = null;
-            //foreach (IPAddress _IPAddress in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
-            //{
-            //    if (_IPAddress.AddressFamily.ToString() == "InterNetwork")
-            //    {
-            //        ip = _IPAddress;
-            //        ipe = new IPEndPoint(ip, port);//把ip和端口转化为IPEndPoint实例
-            //    }
-            //}
-            ipe = new IPEndPoint(ip, port);//把ip和端口转化为IPEndPoint实例
-            c = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//创建一个Socket
+            ipe = new IPEndPoint(ip, port);
+            c = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//Create a Socket
             c.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-            this.richTextBox.Dispatcher.Invoke(new WriteDelegate(ShowText), "连接到服务端...");
-            c.Connect(ipe);//连接到服务器
+            richTextBox.Dispatcher.Invoke(new WriteDelegate(ShowText), "Connect to Server...");
+            c.Connect(ipe);
             Listen = new Thread(new ThreadStart(this.StartListen));
             Listen.IsBackground = true;
             Listen.Start();
         }
 
-        private void StartListen()
+        private void DrawingLines(string recvStr)
         {
             CultureInfo culture = CultureInfo.InvariantCulture;
             Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            int num = 0;
+            string[] values = recvStr.Split(' ');//new string[4];//line.Split(',');
+            values[0] = (num++).ToString();
+
+            double x = Double.Parse(values[0], culture);
+            double y1 = Double.Parse(values[1], culture);
+            double y2 = Double.Parse(values[2], culture);
+            double y3 = Double.Parse(values[3], culture);
+            double y4 = Double.Parse(values[4], culture);
+
+            Point p1 = new Point(x, y1);
+            Point p2 = new Point(x, y2);
+            Point p3 = new Point(x, y3);
+            Point p4 = new Point(x, y4);
+
+            source1.AppendAsync(Dispatcher, p1);
+            source2.AppendAsync(Dispatcher, p2);
+            source3.AppendAsync(Dispatcher, p3);
+            source4.AppendAsync(Dispatcher, p4);
+        }
+
+        private void StartListen()
+        {
+            //CultureInfo culture = CultureInfo.InvariantCulture;
+            //Assembly executingAssembly = Assembly.GetExecutingAssembly();
             //try
             //{
-                string recvStr = "";
-                byte[] recvBytes = new byte[1024];
-                int bytes = 0;
-                int num = 0;
+            string recvStr = "";
+            byte[] recvBytes = new byte[64];
+            int bytes = 0;
+            int num = 0;
             while (true)
             {
                 bytes = c.Receive(recvBytes, recvBytes.Length, 0);//从服务器端接受返回信息
                 if (bytes > 0)
                 {
-                    recvStr += Encoding.ASCII.GetString(recvBytes, 0, bytes);
-                    this.richTextBox.Dispatcher.Invoke(new WriteDelegate(ShowText), "服务器返回信息：" + recvStr);
-                    string[] values = recvStr.Split(' ');//new string[4];//line.Split(',');
-                    values[0] = (num++).ToString();
+                    recvStr += Encoding.UTF8.GetString(recvBytes, 0, bytes).TrimEnd('\n');
+                    richTextBox.Dispatcher.Invoke(new WriteDelegate(ShowText), "Recv from Server：" + recvStr);
+                    DrawingLines(recvStr);
+                    //string[] values = recvStr.Split(' ');//new string[4];//line.Split(',');
+                    //values[0] = (num++).ToString();
 
                     //double x = Double.Parse(values[0], culture);
                     //double y1 = Double.Parse(values[1], culture);
@@ -113,6 +130,7 @@ namespace raspberrypi_client
 
                     recvStr = "";
                 }
+                Array.Clear(recvBytes, 0, recvBytes.Length);
             }
             //}
             //catch (Exception ex)
@@ -126,9 +144,6 @@ namespace raspberrypi_client
             try
             {
                 InitClient();
-                //Self_send = new Thread(self_send);
-                //Self_send.IsBackground = true;
-                //Self_send.Start();
             }
             catch (Exception ex)
             {
@@ -136,34 +151,15 @@ namespace raspberrypi_client
             }
         }
 
-        private void self_send()
-        {
-            while (true)
-            {
-                string sendStr = "1";
-                byte[] bs = Encoding.ASCII.GetBytes(sendStr);
-                c.Send(bs, bs.Length, 0);
-                Thread.Sleep(1000);
-            }
-        }
-
         private void send_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                this.richTextBox.Dispatcher.Invoke(new WriteDelegate(ShowText), "发送消息到服务端 " + sendMessage.Text);
+                richTextBox.Dispatcher.Invoke(new WriteDelegate(ShowText), "Send to Server: " + sendMessage.Text);
                 string sendStr = sendMessage.Text;
                 byte[] bs = Encoding.ASCII.GetBytes(sendStr);
                 c.Send(bs, bs.Length, 0);
                 sendMessage.Clear();
-
-                //string recvStr = "";
-                //byte[] recvBytes = new byte[1024];
-                //int bytes;
-                //bytes = c.Receive(recvBytes, recvBytes.Length, 0);//从服务器端接受返回信息
-                //recvStr += Encoding.UTF8.GetString(recvBytes, 0, bytes);
-
-                //ShowText("服务器返回信息：" + recvStr);
             }
             catch (ArgumentNullException ex1)
             {
@@ -175,11 +171,28 @@ namespace raspberrypi_client
             }
         }
 
+        private void download_Click(object sender, RoutedEventArgs e)
+        {
+            if (year.Text.Length == 0 || mon.Text.Length == 0 || day.Text.Length == 0) MessageBox.Show("Date information is empty!");
+            else
+            {
+                string errorinfo;
+                string localpath = @"C:\Users\jiang\Desktop\raspberrypi_client";
+                string filename = year.Text + "-" + mon.Text + "-" + day.Text + ".txt";
+                FtpUpDown ftp = new FtpUpDown(xip.Text, "pi", "raspberry");
+                bool bol = ftp.Download(localpath, filename, out errorinfo);
+                if (bol == true)
+                    MessageBox.Show("Download success");
+                else
+                    MessageBox.Show("Download fail：" + errorinfo + "");
+            }
+        }
+
         private delegate void WriteDelegate(string str);
         private void ShowText(string text)
         {
-            this.richTextBox.AppendText(text + "\n");
-            this.richTextBox.ScrollToEnd();
+            richTextBox.AppendText(text + "\n");
+            richTextBox.ScrollToEnd();
 
         }
 
@@ -198,5 +211,71 @@ namespace raspberrypi_client
             System.Diagnostics.Process.GetCurrentProcess().Kill();
             Application.Current.Shutdown();
         }
+    }
+
+    public class FtpUpDown
+    {
+        string ftpServerIP;
+        string ftpUserID;
+        string ftpPassword;
+        FtpWebRequest reqFTP;//FTP requset
+        private void Connect(String path)//Connect ftp
+        {
+            // Create FtpWebRequest objects based uri
+            reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(path));
+            // Specify the type of data transfer
+            reqFTP.UseBinary = true;
+            reqFTP.Credentials = new NetworkCredential(ftpUserID, ftpPassword);
+        }
+        public FtpUpDown(string ftpServerIP, string ftpUserID, string ftpPassword)
+        {
+            this.ftpServerIP = ftpServerIP;
+            this.ftpUserID = ftpUserID;
+            this.ftpPassword = ftpPassword;
+        }
+
+        //DownLoad the file
+        public bool Download(string localpath, string fileName, out string errorinfo)
+        {
+            try
+            {
+                String onlyFileName = Path.GetFileName(fileName);
+                string newFileName = localpath + "\\" + onlyFileName;
+
+                if (File.Exists(newFileName))
+                {
+                    errorinfo = string.Format("Local File {0} already exists, can not be downloaded", newFileName);
+                    return false;
+                }
+                string url = "ftp://" + ftpServerIP + "//home/pi/mxin/TestFile/" + fileName;
+                Connect(url); 
+                reqFTP.Credentials = new NetworkCredential(ftpUserID, ftpPassword);
+                FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
+                Stream ftpStream = response.GetResponseStream();
+                long cl = response.ContentLength;
+                int bufferSize = 2048;
+                int readCount;
+                byte[] buffer = new byte[bufferSize];
+                readCount = ftpStream.Read(buffer, 0, bufferSize);
+
+                FileStream outputStream = new FileStream(newFileName, FileMode.Create);
+                while (readCount > 0)
+                {
+                    outputStream.Write(buffer, 0, readCount);
+                    readCount = ftpStream.Read(buffer, 0, bufferSize);
+                }
+                ftpStream.Close();
+                outputStream.Close();
+                response.Close();
+                errorinfo = "";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errorinfo = string.Format("Because {0} , can not be downloaded", ex.Message);
+                return false;
+            }
+        }
+
     }
 }
